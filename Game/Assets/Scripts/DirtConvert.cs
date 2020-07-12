ce292d0ab5e2ad332e3f633fc2c60e8537307e1f
Supernova1114 <supernovawarriors128@gmail.com>
2020-07-11 20:25:42 -0700

    ahhh
    dsadasd

---
+++ Game/Assets/Scripts/DirtConvert.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtConvert : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite mudSprite;
+    public Collider2D mudCollider;
+    public Collider2D dirtCollider;
+    public Rigidbody2D body;
+    public float snapTime;
+
+    bool isMud = false;
+    int consumeCount = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (consumeCount < 3)
+        {
+            if (collision.gameObject.tag == "water")
+            {
+                consumeCount++;
+                Destroy(collision.gameObject, 0);
+            }
+        }
+        else
+        {
+            if (isMud == false)
+            {
+                spriteRenderer.sprite = mudSprite;
+
+                StartCoroutine(SetBodyType());
+
+                isMud = true;
+            }
+        }
+    }
+
+    IEnumerator SetBodyType()
+    {
+        dirtCollider.enabled = false;
+        mudCollider.enabled = true;
+
+        body.bodyType = RigidbodyType2D.Dynamic;
+
+        yield return new WaitForSeconds( snapTime );
+
+        body.bodyType = RigidbodyType2D.Static;
+    }
+
+}
---
+++ Game/Assets/Scripts/WaterConvert.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterConvert : MonoBehaviour
+{
+    public Collider2D colliderr;
+    public GameObject acid;
+
+    private bool hasCollided = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ( collision.gameObject.tag == "acid")
+        {
+            if (hasCollided == false)
+            {
+                hasCollided = true;
+
+                //colliderr.enabled = false;
+                Instantiate(acid, transform.position, transform.rotation);
+
+                //collision.gameObject.GetComponent<snap>().Snap();
+
+                Destroy(gameObject, 0f);
+            }
+        }
+    }
+
+
+}
